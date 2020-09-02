204d7180dbda34196987dd5201de7b7dad8ac3df
anton <uawesome13@gmail.com>
2020-09-02 16:59:57 +0300

    home page output content
    playlists can be extracted from database with easy sorting and be shown in home page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
+from accounts.models import User
 
-# Create your models here.
+
+class Playlist(models.Model):
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=260)
+    date = models.DateTimeField(default=datetime.now(), editable=False)
+    likes = models.IntegerField(default=0, editable=False)
+
+    def __str__(self):
+        return "%s by %s" % (self.title, self.date)
--- home/views.py
+++ home/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from .models import Playlist
+from django.http import JsonResponse
+import re
 
 
 class HomeView(View):
@@ -15,11 +18,26 @@ class HomeView(View):
 
     def post(self, request):
         user_authenticated = request.user.is_authenticated
-        name = request.POST.get("name", "")
+        playlists_keys = request.POST.get("playlists_keys", "")
+        page_content = list(Playlist.objects.values())
+        def amount_of_occurences(str):
+            general_amount = 0
+            for key in playlists_keys.split():
+                general_amount += len(re.findall(key, str['title']))
+                general_amount += len(re.findall(key, str['description']))
+                general_amount += str['likes']
+            return general_amount
+        page_content.sort(key = lambda a: (amount_of_occurences(a), int(a['likes'])), reverse=True)
+        if not page_content:
+            page_header = "Nothing found"
+            page_content = "List is empty"
+        else:
+            page_header = "Playlist's list"
+        # page_content = playlists_keys
         return render(
             request,
             "home.html",
-            context={"user_authenticated": user_authenticated, "active_page": "home", "name": name},
+            context={"user_authenticated": user_authenticated, "active_page": "home", "page_header": page_header, "page_content": page_content},
         )
 
 

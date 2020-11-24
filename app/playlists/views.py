b6e012db3af38fbb045a88afa0527141c093dcd3
anton <uawesome13@gmail.com>
2020-11-24 22:29:00 +0200

    fix(search playlist): add levenshtein distance instead primitive algorithm

--- app/playlists/views.py
+++ app/playlists/views.py
@@ -87,22 +87,46 @@ class ShowPlaylistsView(View):
                 from whole load. Take str with string of keys and playlists.
             """
 
-            def amount_of_occurences(playlist):
-                general_amount = 0
-                for key in playlists_keys.split():
-                    general_amount += len(
-                        re.findall(key, playlist["title"])
-                    )
-                    general_amount += len(
-                        re.findall(key, playlist["description"])
-                    )
-                return general_amount
+            def distance(a, b):
+                "Calculates the Levenshtein distance between a and b."
+                n, m = len(a), len(b)
+                if n > m:
+                    # Make sure n <= m, to use O(min(n, m)) space
+                    a, b = b, a
+                    n, m = m, n
 
+                current_row = range(n + 1)  # Keep current and previous row, not entire matrix
+                for i in range(1, m + 1):
+                    previous_row, current_row = current_row, [i] + [0] * n
+                    for j in range(1, n + 1):
+                        add, delete, change = (
+                            previous_row[j] + 1,
+                            current_row[j - 1] + 1,
+                            previous_row[j - 1]
+                        )
+                        if a[j - 1] != b[i - 1]:
+                            change += 1
+                        current_row[j] = min(add, delete, change)
+
+                return current_row[n]
+        
             playlists.sort(
-                key=lambda a: (amount_of_occurences(a), int(a["likes"])),
-                reverse=True
+                key=lambda a: (
+                    (
+                        distance(a['title'], playlists_keys)*0.7 + 
+                        distance(a['description'], playlists_keys)*0.1
+                    ), 
+                    -int(a['likes'])
+                ),
             )
-            playlists = list(filter(amount_of_occurences, playlists))
+            
+            playlists = filter(
+                lambda a: (
+                    distance(a['title'], playlists_keys)
+                ) < 0.9 * len(a['title']),
+                playlists
+            )
+            
             return playlists
 
         user_authenticated = request.user.is_authenticated
